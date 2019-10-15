ecd18f9a278590f8b09205a5021cec80e4286a15
Nadav Green <nadavgreen@Nadavs-MBP.fios-router.home>
2019-10-14 21:43:19 -0400

    allow cors

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const getApp = () => {
 	app.use(require('body-parser').json());
 	app.use((req, res, next) => {
 		res.header("Access-Control-Allow-Origin" , "*");
-		//res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-		//res.header("Access-Control-Allow-Methods", "PUT, DELETE")
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		res.header("Access-Control-Allow-Methods", "GET", "POST", "PUT, DELETE")
 		next()
 	});
 
